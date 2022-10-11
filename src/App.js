83a6627d18b2625ceb79014d0e0fede57f853386
mdYeasin-cmd <yeasin200011@gmail.com>
2022-10-11 22:25:50 +0600

    React Router added in project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import Main from './layouts/Main';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+    }
+  ])
+
   return (
     <div className="App">
-      
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
