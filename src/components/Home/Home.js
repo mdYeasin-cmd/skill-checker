9965f8663ab2eea402ee631ed2e50a83d4c7bfaf
mdYeasin-cmd <yeasin200011@gmail.com>
2022-10-12 22:50:20 +0600

    Slight unuses code removed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useActionData, useLoaderData } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 import Banner from '../Banner/Banner';
 import Footer from '../Footer/Footer';
 import Technologies from '../Technologies/Technologies';
